940a67cfe6ed9c3775bb7875b2e624090c240a40
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-28 15:56:02 +0000

    Deploy siggame/Cerveau to github.com/siggame/Cerveau.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,19 @@
+define({
+  "name": "Cerveau API",
+  "version": "1.0.0",
+  "description": "API for the Cerveau Game Server.",
+  "title": "Cerveau API",
+  "url": "http://localhost:3080",
+  "template": {
+    "withCompare": false
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-28T15:55:49.372Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
